27f12b93f364a6d90972ac97f89156d5ec7badb8
Angel Martinez <aml@geezar.es>
2019-05-13 11:52:00 +0200

    YA funciona el reduce con el parche que por lo visto hay que usar siempre

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -13,14 +13,23 @@ listaPares = filter(lambda x: x%2==0, lista)
 
 # HAcemos el sumaTodos de otros ejercicios con un Reduce
 sumatorio=reduce(lambda x,y : x+y, lista)
+
 #Ojo con este, el primero lo coge tal cual pero no lo doble, porque l reduce es as´.
 # Coger x como el primero y actuar sobre el resto
-sumatorioDoble = reduce(lambda x,y: x + y*2, lista)
+# EN RESUMEN: REDUCE NO HACE LA FUNCION SOBRE EL PRIMER ELEMENTO
+sumatorioDobleMal = reduce(lambda x,y: x + y*2, lista)
+
+# PARA CORREGIR EL REDUCE, HACEMOS COPIA DE LA LISTA CON UN 0 EN EL PRIMER ELEMENTO
+listaParaReduce=lista[:]
+listaParaReduce.insert(0,0)
+sumatorioDobleCorrecto= reduce(lambda x,y: x + y*2, listaParaReduce)
 
 
+# Saco resultados para que se vean en fase de pruebas
 print(lista)
 print(list(listaRomanos))
 print(list(listaPares))
 print(sumatorio)
-print(sumatorioDoble)
+print(sumatorioDobleMal)
+print(sumatorioDobleCorrecto)
 
