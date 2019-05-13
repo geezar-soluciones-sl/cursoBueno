6b7d4ff62dc6cec44750b55b9e29c73e99f643a4
Angel Martinez <aml@geezar.es>
2019-05-13 18:38:41 +0200

    Creamos clases y clases heredadas, incluso con atributos privados y los consiguientes getter y setter

--- objetoPerro.py
+++ objetoPerro.py
@@ -16,3 +16,20 @@ class Perro():
     def __str__(self):
         return "Mi nombre es {}".format(self.nombre)
     
+class PerroGuia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo=amo
+        self.trabajando = False
+        
+    def __str__(self):
+        return "Mi nombre es {} y soy un perro de asistencia".format(self.nombre)
+    
+    def pasear(self):
+        print("Vamos a dar una vuelta")
+        
+    def ladrar(self):
+        if not self.trabajando:
+            Perro.ladrar(self)
+        else:
+                print("No puedo ladrar, estoy trabajando")
