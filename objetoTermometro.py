f64df6317ec5d8bb289941a7668b3e31d46a58d8
Angel Martinez <aml@geezar.es>
2019-05-14 18:10:12 +0200

    Ponemos atributos privados, getters, setters y función mide que recibe en qué unidades lo quiere

--- objetoTermometro.py
+++ objetoTermometro.py
@@ -1,14 +1,17 @@
 # Nuestro objeto será un termometro con opción de escalas.
 # Como atrobutos, tendrá el valor y la escala, que serán 0ºC por defecto.
 # Como métodos, tendrá un conversor entre Celsius y Fahrenheit.
+# Añadimos getters y setters.
+# Añadimos método "mide" para salida en unidades concretas
+# Transformamos el conversor en privado
 
 class Termometro():
     
     def __init__(self):
-        self.unidades="C"
-        self.temperatura=0
+        self.__unidades="C"
+        self.__temperatura=0
         
-    def conversor(self, temperatura,unidades):
+    def __conversor(self, temperatura,unidades):
         if unidades == "C":
             t = temperatura * 9 / 5 +32
             return "{} F".format(t)
@@ -19,6 +22,26 @@ class Termometro():
             return "Unidades incorrectas"
         
     def __str__(self):
-        return "{}º {}".format(self.temperatura,self.unidades)
+        return "{}º {}".format(self.__temperatura,self.__unidades)
     
-    
+# Ponemos getter y setter para cada atributo
+    def unidadMedida(self, unidad=None):
+        if unidad == None:
+            return self.__unidades
+        else:
+            if unidad == "F" or unidad == "C":
+                self.__unidades = unidad
+
+    def temp(self, temp=None):
+        if temp == None:
+            return self.__temperatura
+        else:
+            self.__temperatura = temp
+# Método para devolver la medida
+    def mide(self, unid=None):
+        if unid == None or unid == self.__unidades:
+            return self.__str__()
+        else:
+            return self.__conversor(self.__temperatura, self.__unidades)
+        
+
